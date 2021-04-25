c0aed372907691d5397f54004560d3301766bd7a
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2021-04-25 16:52:43 +0530

    frontend added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import MaterialTable from "material-table";
+import data from './data/out.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div>
+            <MaterialTable
+            title="GSOC Frequency Viewer"
+            columns={[
+                {title: 'Organizations', field: 'Organizations'},
+                {title: 'About', field: 'About'},
+                {title: 'Category', field: 'Category'},
+                {title: 'Technology', field: 'Technology'},
+                {title: 'Topics', field: 'Topics'},
+                {title: 'Count', field: 'Count'},
+                {title: 'Last Appeared', field: 'Last Appeared'},
+            ]}
+            data={data}
+            options={{
+                paging: true,
+                pageSize: 20,       // make initial page size
+                emptyRowsWhenPaging: true,   //to make page size fix in case of less data rows
+                pageSizeOptions: [20, 30, 50],
+                exportButton: true// rows selection options
+            }}
+        />
+        </div>
+
+    )
 }
 
 export default App;
