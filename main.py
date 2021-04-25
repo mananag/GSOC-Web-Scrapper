e341a746caa095c8c1e14c78d0fcb837b1aa8748
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2021-04-25 15:14:47 +0530

    json file created

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from collections import Counter
 
 import pandas
 
-
 df = {}
 
 for year in reversed(range(2009, 2021)):
@@ -20,15 +19,17 @@ for year in df:
     print(year)
     for item in df[year]:
         for key in combined:
-            if(key != 'year'):
+            if (key != 'year'):
                 try:
                     combined[key].append(item[key])
                 except:
                     combined[key].append(None)
         combined['year'].append(year)
 
+
 def remove(string):
     return "".join(string.split())
+
 
 org = Counter()
 for organization in combined['Organizations']:
@@ -39,19 +40,21 @@ org = dict(org)
 final = []
 
 for organization in sorted(org.keys()):
-    for i, j, k, l, m, n in zip(combined['Organizations'], combined["About"], combined['Technology'], combined['Category'],
-                             combined['Topics'], combined['year']):
+    for i, j, k, l, m, n in zip(combined['Organizations'], combined["About"], combined['Technology'],
+                                combined['Category'],
+                                combined['Topics'], combined['year']):
         if organization == remove(i).lower():
             final.append({
-            "Organizations": i,
-            "About": j,
-            "Technology": k,
-            "Category": l,
-            "Topics": m,
-            "Count": org[organization],
-            "Last Appeared": n
+                "Organizations": i,
+                "About": j,
+                "Technology": k,
+                "Category": l,
+                "Topics": m,
+                "Count": org[organization],
+                "Last Appeared": n
             })
             break
 
-
 pandas.DataFrame(final).to_csv("out.csv")
+
+pandas.DataFrame(final).to_json("client//src//data//out.json", orient='records')
